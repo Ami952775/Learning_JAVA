84954752b54c1acf0fc75a0fc047a049eec1aca8
Amit Kadam <kadamamit1307@gmail.com>
2024-08-24 21:55:25 +0530

    add all

---
+++ src/EG_11072024.java
@@ -0,0 +1,2 @@
+public class EG_11072024 {
+}
---
+++ src/EG_12072024/LAB001.java
@@ -0,0 +1,12 @@
+package EG_12072024;
+
+import com.sun.tools.javac.Main;
+
+public class LAB001 {
+    public static void main(String[] args) {
+        System.out.println("2");
+        
+    }
+
+
+}
